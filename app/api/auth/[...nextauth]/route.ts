cd3691758fd77163301946b73555149228183c54
Kcruiser <davydavidson777@proton.me>
2025-05-13 14:37:01 -0400

    env fix

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -14,6 +14,8 @@ const GOOGLE_SCOPES = [
   "https://www.googleapis.com/auth/calendar.events",
   "https://www.googleapis.com/auth/calendar.readonly",
   "https://www.googleapis.com/auth/calendar.settings.readonly",
+  "https://www.googleapis.com/auth/drive",           // Full Drive access (read/write)
+  "https://www.googleapis.com/auth/drive.file",      // Access to files created/opened by app
   "https://www.googleapis.com/auth/drive.readonly",
   "https://www.googleapis.com/auth/drive.metadata.readonly"
 ].join(" ")
